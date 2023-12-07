79fb8dc50f1f6bc018c542b135c61d0262a82b9e
Qii <57295427+syauqqii@users.noreply.github.com>
2023-12-07 23:08:38 +0700

    gatau lupa update apa tadi

--- module/controller.py
+++ module/controller.py
@@ -2,5 +2,4 @@
 # from controller.analytics import Analytics
 # from controller.blibli import Blibli
 from controller.driver import Driver
-# from controller.tokopedia import Tokopedia
 from controller.shopee import Shopee
